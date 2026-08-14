6702fa41741391086a47e64e6d3852b7060df6cd
borishejblum <boris.hejblum@gmail.com>
2026-08-14 02:21:57 +0200

    accent on Thiébaut in UTF-8

--- R/citcdf-package.R
+++ R/citcdf-package.R
@@ -13,7 +13,7 @@
 #'   \item \code{\link{plot_compare_ccdf}}: diagnostic CCDF plots.
 #' }
 #'
-#' @references Gauthier M, Agniel D, Thiebaut R & Hejblum BP (2021).
+#' @references Gauthier M, Agniel D, Thiébaut R & Hejblum BP (2021).
 #' Distribution-free complex hypothesis testing for single-cell RNA-seq
 #' differential expression analysis, \emph{bioRxiv} 445165.
 #' \doi{10.1101/2021.05.21.445165}
